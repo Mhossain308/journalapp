3dda298164ff91814585a9d8f5bca916a0b27599
Christopher Read <Christopherread22@outlook.com>
2021-03-09 16:55:55 -0500

    updated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,8 +4,25 @@ class CategoriesController < ApplicationController
     end
 
     def show
-        @category = Category.find(params[:id])
-        @journal_entries =  JournalEntry.all
+        @category = Category.find(params[:id]) 
+        @journal_entries = @category.journal_entries
+    end
+
+    # def update
+    #     @category = Category.find(params[:id])
+    #     @category = Category.update(category_params)
+    # end
+
+    # def delete
+    #     @category = Category.find(params[:id])
+    #     @category.destroy
+    #     redirect_to categories_path
+    # end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 
 end
--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -6,22 +6,36 @@ class JournalEntriesController < ApplicationController
 
     def show
         @journal_entry = JournalEntry.find(params[:id])
+        # @categories = @JournalEntry.categories
     end
 
     
     def new 
-    @journal_entry = JournalEntry.new
+        @categories = Category.all
+        @journal_entry = JournalEntry.new
     end
     
-    # def create
-    
-    # end
+    def create
+        @journal_entry = JournalEntry.create(journal_entry_params)
+        redirect_to journal_entries_path
+    end
+
+    def update
+        @journal_entry = JournalEntry.find(params[:id])
+        @journal_entry = JournalEntry.update(journal_entry_params)
+    end
+
+    def delete
+        @journal_entry = JournalEntry.find(params[:id])
+        @journal_entry.destroy
+        redirect_to 
+    end
 
     private
 
-    # def journal_entry_params 
-    #     params.require(:journal_entries).permit(:username, :password)
-    #    end
+    def journal_entry_params 
+        params.require(:journal_entry).permit(:user, :category, :content)
+       end
     
 
 
