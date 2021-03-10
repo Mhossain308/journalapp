25de4bec7c038b364b6223829fe99209b9fbdd5e
Christopher Read <Christopherread22@outlook.com>
2021-03-10 17:20:42 -0500

    fixing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
         redirect_to categories_path(@user)
     end
 
+    # def handle_login
+    #     @user = User.find_by(username: params[:username])
+    #     if @user && @user.authenticate(params[:password])
+    #         redirect_to "/"
+    #     else
+    #         redirect_to login_path
+    #       end
+    #     end
+
     private 
 
    def user_params 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :journal_entries
   resources :categories
   resources :users
+
+  post "/handle_login", to: 'users#handle_login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
